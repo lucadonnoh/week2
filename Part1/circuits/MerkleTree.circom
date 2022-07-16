5bac3e522f87c7631798a53934238f03d1980f51
lucadonnoh <donnoh99@gmail.com>
2022-07-16 13:27:47 +0200

    week 2 assignment

--- Part1/circuits/MerkleTree.circom
+++ Part1/circuits/MerkleTree.circom
@@ -1,12 +1,32 @@
 pragma circom 2.0.0;
 
 include "../node_modules/circomlib/circuits/poseidon.circom";
+include "../node_modules/circomlib/circuits/mux1.circom";
 
 template CheckRoot(n) { // compute the root of a MerkleTree of n Levels 
     signal input leaves[2**n];
     signal output root;
 
     //[assignment] insert your code here to calculate the Merkle root from 2^n leaves
+    var n_hashes = 2**n-1;
+    var hash[n_hashes];
+
+    // hashing leaves
+    for (var i = 0; i < 2**(n-1); i++) {
+        hash[i] = Poseidon(2);
+        hash[i].inputs[0] <== leaves[i * 2];
+        hash[i].inputs[1] <== leaves[i * 2 + 1];
+    }
+
+    var j = 0;
+    for(var i = 2**(n-1); i < n_hashes; i++) {
+        hash[i] = Poseidon(2);
+        hash[i].inputs[0] <== hash[j * 2].out;
+        hash[i].inputs[1] <== hash[j * 2 + 1].out;
+        j++;
+    }
+
+    root <== hash[n_hashes-1].out;
 }
 
 template MerkleTreeInclusionProof(n) {
@@ -16,4 +36,35 @@ template MerkleTreeInclusionProof(n) {
     signal output root; // note that this is an OUTPUT signal
 
     //[assignment] insert your code here to compute the root from a leaf and elements along the path
+    component hash[n];
+    component mux[n];
+
+    hash[0] = Poseidon(2);
+    mux[0] = MultiMux1(2);
+
+    mux[0].c[1][0] <== path_elements[0];
+    mux[0].c[0][0] <== leaf;
+    mux[0].c[1][1] <== leaf;
+    mux[0].c[0][1] <== path_elements[0];
+
+    mux[0].s <== path_index[0];
+
+    hash[0].inputs[0] <== mux[0].out[0];
+    hash[0].inputs[1] <== mux[0].out[1];
+
+    for(var i = 1; i < n; i++) {
+        hash[i] = Poseidon(2);
+        mux[i] = MultiMux1(2);
+
+        mux[i].c[1][0] <== path_elements[i];
+        mux[i].c[0][0] <== hash[i-1].out;
+        mux[i].c[1][1] <== hash[i-1].out;
+        mux[i].c[0][1] <== path_elements[i];
+
+        mux[i].s <== path_index[i];
+        hash[i].inputs[0] <== mux[i].out[0];
+        hash[i].inputs[1] <== mux[i].out[1];
+    }
+
+    root <== hash[n-1].out;
 }
--- Part1/contracts/MerkleTree.sol
+++ Part1/contracts/MerkleTree.sol
@@ -9,12 +9,52 @@ contract MerkleTree is Verifier {
     uint256 public index = 0; // the current index of the first unfilled leaf
     uint256 public root; // the current Merkle root
 
+    uint public constant depth = 3;
+
     constructor() {
         // [assignment] initialize a Merkle tree of 8 with blank leaves
+        hashes = new uint256[](2**(depth+1)-1);
+        for(uint i = 0; i < 8; i++) {
+            hashes[i] = 0;
+        }
+        uint256 l1 = PoseidonT3.poseidon([hashes[0], hashes[1]]);
+        for(uint i = 8; i < 12; i++) {
+            hashes[i] = l1;
+        }
+        uint256 l2 = PoseidonT3.poseidon([hashes[8], hashes[9]]);
+        for(uint i = 12; i < 14; i++) {
+            hashes[i] = l2;
+        }
+        uint256 l3 = PoseidonT3.poseidon([hashes[12], hashes[13]]);
+        hashes[14] = l3;
+        root = hashes[hashes.length - 1];
     }
 
     function insertLeaf(uint256 hashedLeaf) public returns (uint256) {
         // [assignment] insert a hashed leaf into the Merkle tree
+        require(index < 8);
+        hashes[index] = hashedLeaf;
+
+        uint i = index;
+        for(uint l = 0; l < depth; l++) {
+            uint even;
+            uint odd;
+            if(i % 2 == 0) {
+                even = i;
+                odd = i + 1;
+            } else {
+                even = i - 1;
+                odd = i;
+            }
+            uint256 h = PoseidonT3.poseidon([hashes[even], hashes[odd]]);
+            uint father = even / 2 + 8;
+            hashes[father] = h;
+            i = father;
+        }
+
+        index++;
+        root = hashes[hashes.length - 1];
+        return root;
     }
 
     function verify(
@@ -25,5 +65,9 @@ contract MerkleTree is Verifier {
         ) public view returns (bool) {
 
         // [assignment] verify an inclusion proof and check that the proof root matches current root
+        bool isVerified = verifyProof(a, b, c, input);
+        bool doesRootMatch = input[0] == root;
+        return isVerified && doesRootMatch;
     }
 }
+
